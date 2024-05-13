71793e2bcdd6f6c5c354cef543bca4dc558ab410
shalimar-mehra <shalimarmehraa@gmail.com>
2024-05-13 09:47:25 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,76 @@
+# Data Types & Variables -----------------------------------------------------------------------------------------------
+
+# FirstNumber = int(input("Enter Your First No. : "))
+# SecondNumber = int(input("Enter Your Second No. : "))
+#
+# SumOfFirstAndSecondNumber = FirstNumber + SecondNumber
+#
+# print("Sum of First & Second Number is : ", SumOfFirstAndSecondNumber)
+
+# AreaOfSquare = float(input("Enter the Side of Square : "))
+#
+# print("Area of Square is :", AreaOfSquare * AreaOfSquare)
+
+# FirstNumber = float(input("Enter your first Number : "))
+# SecondNumber = float(input("Enter your Second Number : "))
+#
+# AverageOfFirstAndSecondNumber = (FirstNumber + SecondNumber / 2)
+#
+# print("Average of First & Second Number :", AverageOfFirstAndSecondNumber)
+
+# a = int(input("Enter your first Number : "))
+# b = int(input("Enter your second Number : "))
+#
+# print(a >= b)
+
+# String & Conditional Statement ---------------------------------------------------------------------------------------
+
+# str = "error"
+# print(str.endswith("or"))
+#
+# print(str.capitalize())
+#
+# print(str.replace("e", "o"))
+#
+# print(str.find("r"))
+#
+# print(str.count("r"))
+
+# first = input("Enter Your Name : ")
+# print(len(first))
+
+# First = "$$$$$"
+# print(First.count("$"))
+
+# marks = int(input("Enter Your Marks : "))
+#
+# if marks > 100:
+#     print("Number should be in Between 0 to 100 !")
+# elif marks >= 90:
+#     print("Grade = A")
+# elif marks >= 80:
+#     print("Grade = B")
+# elif marks >= 70:
+#     print("Grade = C")
+# elif marks < 70:
+#     print("Grade = D")
+
+# a = int(input("Enter a Number : "))
+# b = a%2
+#
+# if b == 0:
+#     print("It's Even Number")
+# elif b == 1:
+#     print("It's a Odd Number")
+
+#  List & Tuples -------------------------------------------------------------------------------------------------------
+
+marks1 = 45.23
+marks2 = 86.45
+marks3 = 45.12
+marks4 = 45.45
+
+marks = [45, 78.87646, 78, 78.4]
+
+print(marks[1])
+print(type(marks))
