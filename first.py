37c2b2deb7142ea539fd97a1f424ec200f44743a
shalimar-mehra <shalimarmehraa@gmail.com>
2024-05-20 11:03:37 +0530

    commit25

--- first.py
+++ first.py
@@ -273,3 +273,21 @@
 #     if num[a] == x:
 #         print("Found x Value at index no : ", a )
 #     a += 1
+
+# Break & Continue
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+# print("End the loop")
+
+i = 0
+while i <= 5:
+    if i == 3:
+        i += 1
+        continue
+    print(i)
+    i += 1
