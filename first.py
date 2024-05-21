03d32c8716b879533279466d388978d0d5f6c701
shalimar-mehra <shalimarmehraa@gmail.com>
2024-05-21 12:15:53 +0530

    commit30

--- first.py
+++ first.py
@@ -368,3 +368,20 @@
 # print_list(heroes)
 # print()
 
+# n = 5
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+#
+# cal_fact(5)
+
+# def converter(usd_val):
+#     inr_val = usd_val * 86
+#     print(usd_val, "USD =", inr_val, "INR")
+#
+# converter(873)
+
+# Recursion
+
